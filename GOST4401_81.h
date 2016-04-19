e7c8f43c9f04f042e95300830984aa85cfc49bc2
realspinner <realspinner@gmail.com>
2016-04-19 21:14:00 +0300

    Update GOST4401_81.h
    Cleanup the code

--- GOST4401_81.h
+++ GOST4401_81.h
@@ -16,15 +16,15 @@
 
 
 // Constants
-#define  GOST4401_G		 9.80665
-#define  GOST4401_R    287.05287
-#define  GOST4401_E      6356766
+#define  GOST4401_G	9.80665
+#define  GOST4401_R	287.05287
+#define  GOST4401_E	6356766
 
-#define GOST4401_MIN_PRESSURE	  6.69384
-#define GOST4401_MAX_PRESSURE   101325.00
+#define GOST4401_MIN_PRESSURE	6.69384
+#define GOST4401_MAX_PRESSURE	101325.00
 
-#define GOST4401_MIN_GPALT			0.00
-#define GOST4401_MAX_GPALT		51000.00
+#define GOST4401_MIN_GPALT	0.00
+#define GOST4401_MAX_GPALT	51000.00
 
 
 // Export functions
@@ -35,17 +35,17 @@ float GOST4401_getTemperature(float altitude);
 
 // Structure for lookup table record
 typedef struct __attribute__((packed)) _GOST4401_RECORD{
-	float	  alt;		// Geopotentional altitude
-	float    temp;	    // degrees K
-	float  t_grad;		// degrees K per meter
-	float	press;		// pascals
+	float	  alt;	// Geopotentional altitude
+	float    temp;	// degrees K
+	float  t_grad;	// degrees K per meter
+	float	press;	// pascals
 } GOST4401_RECORD;
 
 // Lookup table with averaged empirical parameters for 
 // lower layers of atmosphere in accordance with GOST 4401-81
 #define GOST4401_LUT_RECORDS 	6
 static const GOST4401_RECORD ag_table[] = {
-  {	 	0,  288.15, -0.0065, 101325.00 },
+  {	0,  288.15, -0.0065, 101325.00 },
   { 11000,  216.65,     0.0,  22632.04 },
   { 20000,  216.65,  0.0010,   5474.87 }, 
   { 32000,  228.65,  0.0028,  868.0146 },
